d8643599ba8a9fd2b073dd0212630e7b99be010b
Kevin Cordano <kmcordano@gmail.com>
2024-03-01 10:35:12 -0600

    2108: FIrst Palindrome

--- 2024/February/2-13/FirstPalindrome.java
+++ 2024/February/2-13/FirstPalindrome.java
@@ -1,5 +1,5 @@
 
-public class FirstPalindrome_2108 {
+public class FirstPalindrome {
    public static void main(String args[]) {
       String[] words = {"abc","car","ada","racecar","cool"};   
       System.out.println(new Solution().firstPalindrome(words));
