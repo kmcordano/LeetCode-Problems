2ba205a6f7b4a2cfc02371e4b6ae9095bd77e45b
Kevin Cordano <kmcordano@gmail.com>
2024-03-09 09:03:32 -0600

    2540: Minimum Common Value

---
+++ 2024/March/3-9 2540 Minimum Common Value/MinCommonValue.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function(nums1, nums2) {
+    let p1 = 0;
+    let p2 = 0;
+
+    while(p1 < nums1.length && p2 < nums2.length) {
+        if(nums1[p1] == nums2[p2]) {
+            return nums1[p1];
+        }
+        else if(nums1[p1] < nums2[p2]) {
+            p1++;
+        }
+        else {
+            p2++;
+        }
+    }
+
+    return -1;
+};
