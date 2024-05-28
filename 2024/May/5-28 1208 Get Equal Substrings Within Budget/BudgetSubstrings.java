bd765189c044737b83dbea51272132cc79df8e0e
Kevin Cordano <kmcordano@gmail.com>
2024-05-28 10:42:34 -0500

    1208: Get Equal Substrings Within Budget

--- 2024/May/5-28 1208 Get Equal Substrings Within Budget/BudgetSubstrings.java
+++ 2024/May/5-28 1208 Get Equal Substrings Within Budget/BudgetSubstrings.java
@@ -1,36 +1,21 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
         int len = s.length();
-        int[] costs = new int[len];
-        for(int i = 0; i < len; i++) {
-            costs[i] = Math.abs(s.charAt(i) - t.charAt(i));
-            System.out.print(costs[i] + " ");
-        }
-
-        int left, right = 0;
+        int left = 0;
+        int right = 0;
         int maxLen = 0;
         int subSum = 0;
-        for(left = 0; left < len; left++) {
-            if(left >= right) {
-                right = left;
-                subSum = costs[right];
+        for(right = 0; right < len; right++) {            
+            subSum += Math.abs(s.charAt(right) - t.charAt(right));
+
+            while(subSum > maxCost) {
+                subSum -= Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
             }
 
-            while(subSum <= maxCost) {
-                if(right - left + 1 > maxLen) {
-                    maxLen = right - left + 1;
-                }
-                     
-                right++;
-                if(right < len) {
-                    subSum += costs[right];
-                }
-                else {
-                    break;
-                }
-            }
-
-            subSum -= costs[left];
+            if(right - left + 1 > maxLen) {
+                maxLen = right - left + 1;
+            } 
         }
 
         return maxLen;
